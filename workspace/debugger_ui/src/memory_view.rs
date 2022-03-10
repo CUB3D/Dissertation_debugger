a7704bf188d83bd01c08cd325ef5e20b5eb38a1d
CUB3D <callumthom11@gmail.com>
2022-03-10 15:19:51 +0000

    Clean up memory viewer

--- workspace/debugger_ui/src/memory_view.rs
+++ workspace/debugger_ui/src/memory_view.rs
@@ -6,6 +6,8 @@ use imgui::{TableColumnSetup, Ui, Window};
 
 use std::ffi::CString;
 use std::ops::Range;
+use std::os::linux::raw::stat;
+use debugger_core::ProcessState;
 
 #[derive(Default)]
 pub struct WidgetMemoryView {
@@ -15,61 +17,76 @@ define_ui_menu!(WidgetMemoryView, "Memory");
 
 impl InnerRender for WidgetMemoryView {
     fn render_inner(&mut self, state: &mut DebuggerState, ui: &Ui) {
-        if let Some(table) = ui.begin_table_header(
-            "Mem",
-            [
-                TableColumnSetup::new("Address"),
-                TableColumnSetup::new("Hex"),
-                TableColumnSetup::new("ASCII"),
-            ],
-        ) {
-            for state in &state.process_state {
-                for (mem, mem_range) in &state.memory {
-                    for (i, c) in mem.chunks(16).enumerate() {
+        if let Some(tab_bar) = ui.tab_bar("MemoryMaps") {
+            for state in &mut state.process_state {
+                if let Some(tab) = ui.tab_item(format!("Memory ({})", state.process.0)) {
+                    if let Some(table) = ui.begin_table_header(
+                        "Mem",
+                        [
+                            TableColumnSetup::new("Address"),
+                            TableColumnSetup::new("Hex"),
+                            TableColumnSetup::new("ASCII"),
+                        ],
+                    ) {
+                        let mut hit_first_visisble = false;
+                        let mut hit_last_visisble = false;
+                        'memory_sections: for (mem, mem_range) in &state.memory {
+                            for (i, c) in mem.chunks(16).enumerate() {
+                                ui.table_next_column();
 
-                        ui.table_next_column();
-                        ui.text(format!("{:#016X}", mem_range.start + 16 * i));
+                                if hit_last_visisble {
+                                    ui.table_next_row();
+                                    continue;
+                                }
 
-                        if !ui.is_item_visible() {
-                            ui.table_next_row();
-                            continue;
-                        }
+                                ui.text(&format!("{:#016X}:", mem_range.start + 16 * i)[2..]);
 
-                        ui.table_next_column();
+                                if !ui.is_item_visible() {
+                                    ui.table_next_row();
 
-                        for c in c {
-                            ui.text(format!("{:02X}", c));
-                            ui.same_line();
-                        }
-                        ui.table_next_column();
-                        for c in c {
-                            if let Ok(s) = std::str::from_utf8(&[*c]) {
-                                ui.text(s);
-                            } else {
-                                ui.text(".");
+                                    // If we have already hit the first visible line, this is the last visible line, so skip to end
+                                    if hit_first_visisble {
+                                        hit_last_visisble = true;
+                                    }
+
+                                    continue;
+                                } else {
+                                    // If we havn't hit the first visible line yet, track that we just hit it
+                                    if !hit_first_visisble {
+                                        hit_first_visisble = true;
+                                    }
+                                }
+
+                                ui.table_next_column();
+
+                                for c in c {
+                                    ui.text(format!("{:02X}", c));
+                                    ui.same_line();
+                                }
+                                ui.table_next_column();
+                                for c in c {
+                                    if let Some(s) = std::char::from_u32(*c as u32) {
+                                        if s.is_ascii_alphanumeric() || s.is_ascii_punctuation() || s.is_ascii_whitespace() {
+                                            ui.text(s.to_string());
+                                        } else {
+                                            ui.text(".");
+                                        }
+                                    } else {
+                                        ui.text(".");
+                                    }
+                                    ui.same_line();
+                                }
+
+                                ui.table_next_row();
                             }
-                            ui.same_line();
                         }
 
-                        ui.table_next_row();
+                        table.end();
                     }
+                    tab.end();
                 }
             }
-
-            table.end();
+            tab_bar.end();
         }
-
-        // for state in &state.process_state {
-        //     let mut memory_editor = MemoryEditor::<ProcessState>::new()
-        //     .read_only(true)
-        //     .mem_size(state.memory.iter().map(|(m, _)| m.len()).sum());
-        //     .read_fn(|mem, offset| {
-        //         for chunk in mem
-
-        //         mem.read(offset)
-        //     });
-        // }
-
-
     }
 }
